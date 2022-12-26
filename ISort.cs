d64a260ff578ca68b586e974aff24148524322f5
cattarinn <115492716+cattarinn@users.noreply.github.com>
2022-12-26 20:40:57 +0200

    Add files via upload

---
+++ ISort.cs
@@ -0,0 +1,11 @@
+﻿using ConsoleApp9.Models.Activity;
+using System.Collections.Generic;
+
+namespace ConsoleApp9.Models.Interfaces
+{
+    public interface ISort
+    {
+        List<TrainingUnit> Units { get; set; }
+        void Sort();
+    }
+}
