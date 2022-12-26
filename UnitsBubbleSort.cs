0d458139fdaf0f23fe4f210006b50999073bac89
cattarinn <115492716+cattarinn@users.noreply.github.com>
2022-12-26 19:52:57 +0200

    Add files via upload

---
+++ UnitsBubbleSort.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApp9.Models.Activity;
+using ConsoleApp9.Models.Interfaces;
+using System.Collections.Generic;
+
+namespace ConsoleApp9.Models
+{
+    public class UnitsBubbleSort : ISort
+    {
+        public List<TrainingUnit> Units { get; set; }
+
+        public UnitsBubbleSort(List<TrainingUnit> units)
+        {
+            Units = units;
+        }
+
+        public void Sort()
+        {
+            TrainingUnit unit;
+            for (int i = 0; i < Units.Count; i++)
+            {
+                for (int j = 0; j < Units.Count - 1; j++)
+                {
+                    if (Units[j].CaloriesByDay > Units[j + 1].CaloriesByDay)
+                    {
+                        unit = Units[j];
+                        Units[j] = Units[j + 1];
+                        Units[j + 1] = unit;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UnitsInsertionSort.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp9.Models.Activity;
+using ConsoleApp9.Models.Interfaces;
+using System.Collections.Generic;
+
+namespace ConsoleApp9.Sort
+{
+    public class UnitsInsertionSort : ISort
+    {
+        public List<TrainingUnit> Units { get; set; }
+
+        public UnitsInsertionSort(List<TrainingUnit> units)
+        {
+            Units = units;
+        }
+        public void Sort()
+        {
+            int j;
+            TrainingUnit unit;
+            for (int i = 1; i < Units.Count; i++)
+            {
+                unit = Units[i];
+                for (j = i - 1; j >= 0 && Units[j].CaloriesByDay > unit.CaloriesByDay; j--)
+                {
+                    Units[j + 1] = Units[j];
+                }
+                Units[j + 1] = unit;
+            }
+        }
+    }
+}
